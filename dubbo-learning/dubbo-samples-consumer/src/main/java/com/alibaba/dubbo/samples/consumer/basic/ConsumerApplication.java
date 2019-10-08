d8b6a457cbe3072cf50ac8e1e4cb05b38e270595
chengtong <chengtong@treefinance.com.cn>
2019-10-08 17:35:03 +0800

    master

---
+++ dubbo-learning/dubbo-samples-consumer/src/main/java/com/alibaba/dubbo/samples/consumer/basic/ConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.alibaba.dubbo.samples.consumer.basic;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+@SpringBootApplication
+@ImportResource("classpath*:/spring/dubbo-demo-consumer.xml")
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class,args);
+    }
+}
